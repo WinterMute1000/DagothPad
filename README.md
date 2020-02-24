28bdcbd5678596bb3ae5c5a66eb8037eb3211912
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2020-02-24 10:24:26 +0900

    First
    Yes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # DagothPad
  The pad in The best musician in Tamriel . Tribunal will be hate this. 
+ 
+ Sound source by young_scrolls
+ https://www.youtube.com/watch?v=iR-K2rUP86M
---
+++ app/src/main/java/com/morrowwind/redmountaiondagoth/MainActivity.java
@@ -0,0 +1,93 @@
+package com.morrowwind.redmountaiondagoth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+
+import java.lang.reflect.Field;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    static final int MAX_ROWS=10;
+    static final int NUM_COLUMNS=5;
+    private Field[] dagothField=R.raw.class.getFields();
+    private int fieldLength=dagothField.length;
+    private SoundPool dagothSoundPool=new SoundPool(fieldLength,AudioManager.STREAM_MUSIC,0);
+    private int filedIndex=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        //InitializeSoundPool();
+        setContentView(R.layout.activity_main);
+        TableLayout padTable=(TableLayout)findViewById(R.id.padTable);
+        makeTable(padTable);
+
+    }
+
+    /*private void InitializeSoundPool()
+    {
+    }
+    */
+
+    protected void makeTable(TableLayout padTable) //Make rows
+    {
+       int soundNum=fieldLength;
+       //TableRow[] rows=new TableRow[MAX_ROWS];
+       int numRows=0;
+
+       for (int i=0;i< (soundNum % NUM_COLUMNS ==0 ? soundNum / NUM_COLUMNS : soundNum /NUM_COLUMNS +1);i++)
+       {
+           numRows++;
+
+           if (numRows >= MAX_ROWS)
+           {
+               System.err.println("Can't make over MAX_ROWS! ");
+               break;
+           }
+
+           TableRow newTr=new TableRow(this);
+           makeTableRow(newTr);
+           padTable.addView(newTr,new TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+       }
+    }
+
+    protected void makeTableRow(TableRow tr) // Make rows buttons
+    {
+        String resName="@raw";
+        String packageName=this.getPackageName();
+
+        for (int i=0;i<NUM_COLUMNS;i++)
+        {
+            if (filedIndex >=fieldLength)
+            {
+                    System.err.println("Can't make over fieldLength! ");
+                    break;
+            }
+
+            Button newButton=new Button(this);
+            newButton.setText(dagothField[filedIndex].getName());
+            newButton.setLayoutParams(new TableLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+            int soundId=getResources().getIdentifier(resName,dagothField[filedIndex].getName(),packageName); //Get soundFiles ID
+            newButton.setTag(dagothSoundPool.load(this,soundId,1)); //Save sound in the buttonTag
+            newButton.setOnClickListener(this);
+
+            tr.addView(newButton);
+        }
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+
+    }
+}
