515b9cb3511e11e5cc72a295237c99b2cb6aca78
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2020-02-25 13:17:30 +0900

    music play add

--- app/src/main/java/com/morrowwind/redmountaiondagoth/MainActivity.java
+++ app/src/main/java/com/morrowwind/redmountaiondagoth/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
@@ -17,7 +18,7 @@ import java.lang.reflect.Field;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
     static final int MAX_ROWS=10;
-    static final int NUM_COLUMNS=5;
+    static final int NUM_COLUMNS=3;
     private Field[] dagothField=R.raw.class.getFields();
     private int fieldLength=dagothField.length;
     private SoundPool dagothSoundPool=new SoundPool(fieldLength,AudioManager.STREAM_MUSIC,0);
@@ -45,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
        //TableRow[] rows=new TableRow[MAX_ROWS];
        int numRows=0;
 
+       padTable.setBackgroundResource(R.drawable.background);
        for (int i=0;i< (soundNum % NUM_COLUMNS ==0 ? soundNum / NUM_COLUMNS : soundNum /NUM_COLUMNS +1);i++)
        {
            numRows++;
@@ -57,13 +59,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
            TableRow newTr=new TableRow(this);
            makeTableRow(newTr);
-           padTable.addView(newTr,new TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+           padTable.addView(newTr,new TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
        }
     }
 
     protected void makeTableRow(TableRow tr) // Make rows buttons
     {
-        String resName="@raw";
+        String dirName="raw";
         String packageName=this.getPackageName();
 
         for (int i=0;i<NUM_COLUMNS;i++)
@@ -75,11 +77,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
 
             String fileName=dagothField[filedIndex].getName();
+            String fileResourceName="@raw/"+fileName;
 
             Button newButton=new Button(this);
             newButton.setText(fileName);
-            newButton.setLayoutParams(new TableLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT));
-            int soundId=getResources().getIdentifier(resName,fileName,packageName); //Get soundFiles ID
+            newButton.setTextSize(8.5f);
+            int soundId=getResources().getIdentifier(fileResourceName,dirName,packageName); //Get soundFiles ID
             newButton.setTag(dagothSoundPool.load(this,soundId,1)); //Save sound in the buttonTag
             newButton.setOnClickListener(this);
 
@@ -90,8 +93,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View v)
+    public void onClick(View v) //if button, play music!
     {
+        if(v instanceof Button)
+        {
 
+            dagothSoundPool.play((int)v.getTag(),1.0F,1.0F,1,0,1.0F);
+        }
     }
 }
